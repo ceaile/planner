c6b86b8dc751824f07c14d6fe5a41e97d328cdc2
Ceaile <ceaile1994@protonmail.com>
2024-12-14 20:35:58 +0100

    creado proyecto springboot 1 de la api con conf inicial

---
+++ ramen-api/src/main/java/com/ramenrater/ramenapi/RamenApiApplication.java
@@ -0,0 +1,13 @@
+package com.ramenrater.ramenapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RamenApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RamenApiApplication.class, args);
+	}
+
+}
---
+++ ramen-api/src/test/java/com/ramenrater/ramenapi/RamenApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramenrater.ramenapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RamenApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
